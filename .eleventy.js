26da2f65e9f7f2cf5e635d9b52590069b9bc1195
Sylvain Emery <sylvain@emery.sc>
2022-09-16 00:54:37 +0200

    Add basic Eleventy config and structure

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+const filters = require('./utils/filters.js');
+const transforms = require('./utils/transforms.js');
+const shortcodes = require('./utils/shortcodes.js');
+
+module.exports = function (eleventyConfig) {
+  // Filters
+  Object.keys(filters).forEach((filterName) => {
+    eleventyConfig.addFilter(filterName, filters[filterName]);
+  });
+
+  // Transforms
+  Object.keys(transforms).forEach((transformName) => {
+    eleventyConfig.addTransform(transformName, transforms[transformName]);
+  });
+
+  // Shortcodes
+  Object.keys(shortcodes).forEach((shortcodeName) => {
+    eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName]);
+  });
+
+  return {
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      layouts: '_layouts',
+      data: '_data',
+      output: '_site',
+    },
+  };
+};
